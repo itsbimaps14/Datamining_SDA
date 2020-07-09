78619105288e28601ac49d20a046c9e360296c17
itsbimaps14 <61398214+itsbimaps14@users.noreply.github.com>
2020-07-09 07:34:16 +0700

    Extract - Polosan
    Script extract - on going

---
+++ Datamining_SDA/src/Tubes_SDA/Transaction.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Tubes_SDA;
+
+/**
+ *
+ * Author = Bima Putra S | 191524038 | D4-1B "2019
+ * Politeknik Negeri Bandung
+ * Tanggal :
+ * Deskripsi :
+ */
+public class Transaction {
+
+}
